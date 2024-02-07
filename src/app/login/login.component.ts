f0c73ca739742c72f20540dfd3a1363813d245c3
dipaksolanki2 <dipak.solanki@techivies.com>
2024-02-07 18:14:12 +0530

    repo added to github

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  submitted: boolean = false
+
+  onSubmit() {
+    console.log(this.loginForm);
+    
+  }
+}
